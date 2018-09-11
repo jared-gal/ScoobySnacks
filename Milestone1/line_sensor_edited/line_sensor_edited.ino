8074b4a29f53b540192fab63b23fa70883897a64
Larry Tang <ltang500@gmail.com>
2018-09-11 13:10:20 -0400

    combine servo with line senor

--- Milestone1/line_sensor_edited/line_sensor_edited.ino
+++ Milestone1/line_sensor_edited/line_sensor_edited.ino
@@ -12,6 +12,16 @@
 #define SENSOR0_PIN 2
 #define SENSOR1_PIN 3
 
+#include <Servo.h>
+
+const int POT = A0;
+const int SERVO1 = A4;
+const int SERVO2 = A5;
+Servo myServo1;
+Servo myServo2;
+
+const int threshold = 100;
+
 // These variables are marked 'volatile' to inform the compiler that they can change
 // at any time (as they are set by hardware interrupts).
 volatile long SENSOR0_TIMER;
@@ -36,14 +46,14 @@ void setup_sensor(int pin, long *sensor_timer) {
 // this function averages the sensors starting on the line
 void averageLineSensor(int pin){
   if(pin == 2){
-     temp = 0;
+     int temp = 0;
       for(int i = 0; i < 1000; i++){
           temp += (micros()-SENSOR0_TIMER);
         }
       Sensor0_Offset = temp/1000.0;
     }
    else if(pin == 3){
-     temp = 0;
+     int temp = 0;
       for(int i = 0; i < 1000; i++){
           temp += (micros()-SENSOR1_TIMER);
         }
@@ -51,22 +61,60 @@ void averageLineSensor(int pin){
     }
 }
 
+void move_ccw(Servo servo, int speed){
+  Serial.print("CCW speed " );
+  Serial.println(speed);
+  speed = map(speed, 0, 90, 90, 180);
+  speed = constrain(speed, 90, 180);
+  servo.write(speed);
+}
+
+void move_cw(Servo servo, int speed){
+  Serial.print("CW speed " );
+  Serial.println(speed);
+  speed = map(speed, 0, 90, 90, 0);
+  speed = constrain(speed, 0, 90);
+  servo.write(speed);
+}
+
 void SENSOR0_ISR() {
   // The sensor light reading is inversely proportional to the time taken
   // for the pin to fall from high to low. Lower values mean lighter colors.
   SENSOR0_READING = micros() - SENSOR0_TIMER - Sensor0_Offset;
+  // turn right from line
+  Serial.print(SENSOR0_READING);
+  delay(1000);
+  if(SENSOR0_READING < 0) {
+    Serial.println("[ Turning right ]");
+    move_ccw(myServo1, 0);
+    move_ccw(myServo2, 100);
+    delay(650); // 2 sec
+  }
   // Reset the sensor for another reading
   setup_sensor(SENSOR0_PIN, &SENSOR0_TIMER);
 }
 
 void SENSOR1_ISR() {
   SENSOR1_READING = micros() - SENSOR1_TIMER - Sensor1_Offset;
-  
+  //turn left from line
+  Serial.print(SENSOR1_READING);
+  delay(1000);
+  if(SENSOR1_READING < 0) {
+    Serial.println("[ Turning left ]");
+    move_ccw(myServo1, 100);
+    move_ccw(myServo2, 0);
+    delay(650); // 2 sec
+  }
   setup_sensor(SENSOR1_PIN, &SENSOR1_TIMER);
 }
 
 void setup() {
+  //Initialize Servo motors
+  myServo1.attach(SERVO1);
+  myServo2.attach(SERVO2);
   Serial.begin(9600);
+  Serial.println("Starting Servos Arduino");
+  
   averageLineSensor(SENSOR0_PIN);
   Serial.print("Done Averaging");
   // Tell the compiler which pin to associate with which ISR
@@ -78,11 +126,45 @@ void setup() {
   setup_sensor(SENSOR1_PIN, &SENSOR1_TIMER);
 }
 
+//Drive in a figure 8
+void figureEight() {
+  for(int i = 0; i < 3; i++) {
+    //Straight
+    move_cw(myServo1, 100);
+    move_ccw(myServo2, 100);
+    delay(1000); // 1 sec
+
+    //Turn Left
+    move_cw(myServo1, 100);
+    move_ccw(myServo2, 100);
+    delay(1000); // 1 sec
+  }
+  for(int i = 0; i < 4; i++) {
+    //Straight
+    move_cw(myServo1, 100);
+    move_ccw(myServo2, 100);
+    delay(1000); // 1 sec
+
+    //Turn Left
+    move_cw(myServo1, 100);
+    move_ccw(myServo2, 100);
+    delay(1000); // 1 sec
+  }
+}
+
 void loop() {
   // These delays are purely for ease of reading.
   Serial.println("Sensor 0");
   Serial.println(SENSOR0_READING);
   delay(500);
+
+  Serial.println("[ Straight 1 ]");
+  move_cw(myServo1, 50);
+  move_ccw(myServo2, 50);
+  delay(1000); // 1 sec
+
+  //figureEight();
+ 
   /*Serial.println("Sensor 1");
   Serial.println(SENSOR1_READING);
   delay(100);*/
