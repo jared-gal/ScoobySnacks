ba6b1bd2b95886f407c8a837eea0e74066a4e531
jared-gal <43042105+jared-gal@users.noreply.github.com>
2018-09-14 15:09:06 -0400

    Update line_sensor_edited_jared.ino

--- Milestone1/line_sensor_edited_jared/line_sensor_edited_jared.ino
+++ Milestone1/line_sensor_edited_jared/line_sensor_edited_jared.ino
@@ -1,3 +1,5 @@
+
+
 /*
  * How to use the QRE1113 Digital Line Sensor by SparkFun with hardware interrupts
  * https://www.sparkfun.com/products/9454
@@ -11,217 +13,104 @@
 
 #define SENSOR0_PIN 2
 #define SENSOR1_PIN 3
-
 #include <Servo.h>
 
-//Declaring the servo motors for the robot
+Servo myServo0;
 Servo myServo1;
-Servo myServo2;
-
-//**********************update****************
-//an arbitrary threshold for now 
-const int threshold = 100;
-//an arbitrary averaging length
-const int avgLength = 10;
-//*******************************************
-
 // These variables are marked 'volatile' to inform the compiler that they can change
 // at any time (as they are set by hardware interrupts).
-volatile long SENSOR0_TIMER;
-volatile long SENSOR1_TIMER;
+//volatile long SENSOR0_TIMER;
+//volatile long SENSOR1_TIMER;
 
 // Consider smoothing this value with your favorite smoothing technique (exponential moving average?)
-volatile int SENSOR0_READING;
-volatile int SENSOR1_READING;
+volatile int s0_read;
+volatile int s1_read;
 
-//offsets that will be applied to avg the sensor reading
-volatile int SENSOR0_AVG_ARRAY[avgLength];
-volatile int SENSOR1_AVG_ARRAY[avgLength];
-volatile int index0;
-volatile int index1;
+int thresh0 = 500;
+int thresh1 = 200;
 
-// A digital write is required to trigger a sensor reading.
-void setup_sensor(int pin, long *sensor_timer) {
-  *sensor_timer = micros();
-  pinMode(pin, OUTPUT);
-  digitalWrite(pin, HIGH);
-  pinMode(pin, INPUT);
-}
-
-// this function averages the sensors values based on a simple 10 value avg
-double averageLineSensor(int pin){
-  double Sensor_Average = -1;
-  if(pin == 2){
-     int temp = 0;
-      for(int i = 0; i < avgLength; i++){
-          temp += SENSOR0_AVG_ARRAY[i];
-        }
-      Sensor_Average = temp/10.0;
-    }
-   else if(pin == 3){
-     int temp = 0;
-      for(int i = 0; i < avgLength; i++){
-          temp += SENSOR1_AVG_ARRAY[i];
-        }
-      Sensor_Average = temp/10.0;
-    }
-  return Sensor_Average;
-}
-
-//this turns the specified servo motor in the counter clockwise direction
-//90 - 180 when written to servo is ccw, and higher values are faster
-//this function takes in a value from 0 - 90 for speed, with ascending beign faster
-//and maps it to the 90-180 range before sending that to the servo
-void move_ccw(Servo servo, int speed){
-  Serial.print("CCW speed " );
-  Serial.println(speed);
-  speed = map(speed, 0, 90, 90, 180);
-  speed = constrain(speed, 90, 180);
-  servo.write(speed);
-}
-
-//this turns the specified servo motor in the clockwise direction
-//90 - 0 when written to servo is cw, and lower values are faster
-//this function takes in a value from 0 - 90 for speed, with ascending beign faster
-//and maps it to the 90-0 range before sending that to the servo
-void move_cw(Servo servo, int speed){
-  Serial.print("CW speed " );
-  Serial.println(speed);
-  speed = map(speed, 0, 90, 90, 0);
-  speed = constrain(speed, 0, 90);
-  servo.write(speed);
-}
-
-
-//This function is called to cause the robot to move straight
-//It employs the move_ccw and move_cw functions
-//It currently is set to be slow ( speed = 50%)
-void goStraight(){
-  //turn servo one cw to go forwards
-  move_cw(myServo1, 45);
-  //turn servo two ccw to go forwards
-  move_ccw(myServo2,45);
+//Code for the QRE1113 Digital board
+//Outputs via the serial terminal – Lower numbers mean more reflected
+//3000 or more means nothing was reflected
+int readQD(int pin){
+  //Returns value from the QRE1113
+  //Lower numbers mean more refleacive
+  //More than 3000 means nothing was reflected.
+  int diff; 
+  pinMode( pin, OUTPUT );
+  digitalWrite( pin, HIGH );
+  delayMicroseconds(10);
+  pinMode( pin, INPUT );
+  long t = micros()
   
+  //time how long the input is HIGH, but quit after 3ms as nothing happens after that
+  while (digitalRead(pin1) == HIGH && micros() - time < 3000);
+  diff = micros()-time
+  return diff
 }
 
-//This function stops the movement of the robot
-void STOP(){
-  move_cw(myServo1,0);
-  move_cw(myServo2,0);
+// input 0-90
+void move_cw(Servo servo, int speed) {
+  servo.write(90-speed);
 }
 
-//This function slightly corrects the motion of the robot to the right
-//by turning one direction for a brief moment before returning to the normal speed straight
-//this is accomplished by slowing one wheel briefly
-//In this case the the right wheel is slowed by an arbitrary value
-void correctRight(){
-  //find out the current speed of the right wheel, and then slowing it down 
-  move_cw(myServo1, (myServo1.read() -5));
-  //an arbitrary delay to let the slow down have an effect
-  delay(10);
-  //resetting the robot to go straight again
-  goStraight();
+// input 0-90
+void move_ccw(Servo servo, int speed) {
+  servo.write(speed+90);
 }
 
-//This function slightly corrects the motion of the robot to the left
-//by turning one direction for a brief moment before returning to the normal speed straight
-//this is accomplished by slowing one wheel briefly
-//In this case the the left wheel is slowed by an arbitrary value
-void correctLeft(){
-  //find out the current speed of the right wheel, and then slowing it down 
-  move_ccw(myServo2, (myServo2.read() -5));
-  //an arbitrary delay to let the slow down have an effect
-  delay(10);
-  //resetting the robot to go straight again
-  goStraight();
-
+void goStraight() {
+  move_cw(myServo0, 5);
+  move_ccw(myServo1, 5);
 }
 
-
-
-//In the ISR we only update the sensor reading to be fast
-void SENSOR0_ISR() {
-    //read the sensor
-    SENSOR0_READING = micros() - SENSOR0_TIMER;
-    if(index0 >= avgLength){
-       index0 = 0;
-    }
-    SENSOR0_AVG_ARRAY[index0] = SENSOR0_READING;
-    //reset the interrupt
-    setup_sensor(SENSOR0_PIN, &SENSOR0_TIMER);
+void corLeft() {
+  move_cw(myServo0, 6);
+  move_ccw(myServo1, 4);
 }
 
-void SENSOR1_ISR() {
-      //read the sensor
-    SENSOR1_READING = micros() - SENSOR1_TIMER;
-    if(index1 >= avgLength){
-       index1 = 0;
-    }
-    SENSOR1_AVG_ARRAY[index1] = SENSOR1_READING;
-    //reset the interrupt
-    setup_sensor(SENSOR1_PIN, &SENSOR1_TIMER);
+void corRight() {
+  move_cw(myServo0, 4);
+  move_ccw(myServo1, 6);
 }
+
+void turnLeft() {
+  move_cw(myServo0, 5);
+  move_cw(myServo1, 0);
+}
+
+void turnRight() {
+  move_ccw(myServo0, 0);
+  move_ccw(myServo1, 5);
+}
+
+int turnCount;
 
 void setup() {
-  //Initialize Servo motors
-  //assuming that servo 1 is the right wheel servo
-  myServo1.attach(A4);
-  //assuming that servo 2 is the left wheel servo
-  myServo2.attach(A5);
-
-  //initialize serial for debugging
-  Serial.begin(9600);
-  Serial.println("Starting Servos Arduino");
-  
-  //setting up averaging
-  index0 = 0;
-  index1 = 0;
-  
-  // Tell the compiler which pin to associate with which ISR
-  attachInterrupt(digitalPinToInterrupt(SENSOR0_PIN), SENSOR0_ISR, LOW);
-  attachInterrupt(digitalPinToInterrupt(SENSOR1_PIN), SENSOR1_ISR, LOW);
-  
-  // Setup the sensors
-  setup_sensor(SENSOR0_PIN, &SENSOR0_TIMER);
-  setup_sensor(SENSOR1_PIN, &SENSOR1_TIMER);
-
-  //delay some to allow the sensor averaging function to fill
-  delay(10);
+  myServo0.attach(A4);
+  myServo1.attach(A5);
+  turnCount = 0;
+  //states are as follows 
 }
 
-
 void loop() {
-  Serial.println("Average of Sensor 0");
-  Serial.println(averageLineSensor(SENSOR0_PIN));
-  Serial.println("Average of Sensor 1");
-  Serial.println(averageLineSensor(SENSOR1_PIN));
-  Serial.println("end of transaction");
+  //getting the sensor values each loop
+  s0_read = readQD(SENSOR0_PIN);
+  s1_read = readQD(SENSOR1_PIN);
+  //s2_read = readQD(SENSOR2_PIN);
 
-  //uncomment when ready to threshold
-  //the case where the main sensor drops off the line
-  /*
-  if(averageLineSensor(SENSOR0_PIN) > threshold){
-      //if the other sensor then comes on the line we know that the robot is veering left
-      //so we must correct right
-      if(averageLineSensor(SENSOR1_PIN) < threshold){
-          correctRight();
-        }
-        //otherwise we assume the robot is veering right if the sensor 1 isn't detecting tape
-       else if(averageLineSensor(SENSOR1_PIN) > threshold){
-          correctLeft();
-        }
-        //if neither of these are the case we will stop the robot
-       else{
-          STOP();  
-        }
-    }
-    //the case in which the main sensor is still on the line, meaning we continue to go straight
-  else if (averageLineSensor(SENSOR0_PIN) < threshold){
-      goStraight();
-    }
-  //if neither is the case then there is an error and we should stop the robot
-  else{
-      STOP();
-    }
-    */
+  if(s0_read > threshold0 && s1_read > threshold1)
+    goStraight();
+  else if(s0_read < threshold0 && s1_read > threshold1)
+    corLeft();
+  else if(s0_read > threshold0 && s1_read < threshold1)
+    corRight();
+  else if(s0_read < threshold0 && s1_read < threshold1){
+      if(turnCount<4){
+        turnRight();
+        turnCount++;
+      }
+       else
+        turnLeft();
+
 }
