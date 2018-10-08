8e87e90834a18666df67d6feacb91489698cd143
David Pirogovsky <dap263@cornell.edu>
2018-10-08 17:03:44 -0400

    Wall Detection kinda works

--- Milestone2/Wall_Detect/Wall_Detect.ino
+++ Milestone2/Wall_Detect/Wall_Detect.ino
@@ -20,116 +20,175 @@ int output[128];
 const int DIST = A1;
 const int SERVO1 = A4;
 const int SERVO2 = A5;
+
+#define SENSOR0_PIN 2
+#define SENSOR1_PIN 3
+
+Servo myServo0;
 Servo myServo1;
-Servo myServo2;
+// These variables are marked 'volatile' to inform the compiler that they can change
+// at any time (as they are set by hardware interrupts).
+//volatile long SENSOR0_TIMER;
+//volatile long SENSOR1_TIMER;
 
-void move_ccw(Servo servo, int speed){
-  speed = map(speed, 0, 90, 90, 180);
-  speed = constrain(speed, 90, 180);
-  servo.write(speed);
+// Consider smoothing this value with your favorite smoothing technique (exponential moving average?)
+volatile int s0_read;
+volatile int s1_read;
+
+int thresh0 = 500;
+int thresh1 = 200;
+int thresh_wall = 200;
+
+//Code for the QRE1113 Digital board
+//Outputs via the serial terminal – Lower numbers mean more reflected
+//3000 or more means nothing was reflected
+int readQD(int pin){
+  //Returns value from the QRE1113
+  //Lower numbers mean more refleacive
+  //More than 3000 means nothing was reflected.
+  int diff; 
+  pinMode( pin, OUTPUT );
+  digitalWrite( pin, HIGH );
+  delayMicroseconds(10);
+  pinMode( pin, INPUT );
+  long t = micros();
+  
+  //time how long the input is HIGH, but quit after 3ms as nothing happens after that
+  while (digitalRead(pin) == HIGH && (micros() - t) < 3000);
+  diff = micros()-t;
+  return diff;
 }
 
-void move_cw(Servo servo, int speed){
-  speed = map(speed, 0, 90, 90, 0);
-  speed = constrain(speed, 0, 90);
-  servo.write(speed);
+// input 0-90
+void move_cw(Servo servo, int speed) {
+  servo.write(90-speed);
 }
+
+// input 0-90
+void move_ccw(Servo servo, int speed) {
+  servo.write(speed+90);
+}
+
+void goStraight() {
+  move_cw(myServo0, 5);
+  move_ccw(myServo1, 5);
+}
+
+void corLeft() {
+  move_cw(myServo0, 6);
+  move_ccw(myServo1, 2);
+}
+
+void corRight() {
+  move_cw(myServo0, 2);
+  move_ccw(myServo1, 6);
+}
+
+void turnLeft() {
+  move_cw(myServo0, 5);
+  move_cw(myServo1, 0);
+  delay(1950);
+  move_cw(myServo0, 0);
+  move_cw(myServo1, 0);
+  delay(500);
+}
+
+void turnRight() {
+  move_ccw(myServo0, 10);
+  move_ccw(myServo1, 10);
+  delay(750);
+}
+
+int turnCount;
 
 void setup() {
-  //Initialize Servo motors
-  myServo1.attach(SERVO1);
-  myServo2.attach(SERVO2);
-
+  myServo0.attach(A4);
+  myServo1.attach(A5);
+  //turnCount = 0;
+  //states are as follows 
+  
   //TIMSK0 = 0; // turn off timer0 for lower jitter
-  //ADCSRA = 0xe5; // set the adc to free running mode
-  //ADMUX = 0x40; // use adc0
-  //DIDR0 = 0x01; // turn off the digital input for adc0
-  //pinMode(A0, INPUT);
+  ADCSRA = 0xe5; // set the adc to free running mode
+  ADMUX = 0x40; // use adc0
+  DIDR0 = 0x01; // turn off the digital input for adc0
+  DIDR1 = 0x01; // turn off the digital input for adc1
   pinMode(DIST, INPUT);
+  //Serial.begin(9600);
 }
 
-//Drive in a figure 8
-void figureEight() {
-  for(int i = 0; i < 3; i++) {
-    //Straight
-    move_cw(myServo1, 100);
-    move_ccw(myServo2, 100);
-    delay(1000); // 1 sec
-
-    //Turn Left
-    move_cw(myServo1, 100);
-    move_ccw(myServo2, 100);
-    delay(1000); // 1 sec
-  }
-  for(int i = 0; i < 4; i++) {
-    //Straight
-    move_cw(myServo1, 100);
-    move_ccw(myServo2, 100);
-    delay(1000); // 1 sec
-
-    //Turn Left
-    move_cw(myServo1, 100);
-    move_ccw(myServo2, 100);
-    delay(1000); // 1 sec
-  }
+signed int adc_read() {
+  while(!(ADCSRA & 0x10)); // wait for adc to be ready
+  ADCSRA = 0xf5; // restart adc
+  byte m = ADCL; // fetch adc data
+  byte j = ADCH;
+  int k = (j << 8) | m; // form into an int
+  k -= 0x0200; // form into a signed int
+  k <<= 6; // form into a 16b signed int
+  return k;
 }
 
 void loop() {
   // These delays are purely for ease of reading.
-  
+  while(1) { // reduces jitter
+    //cli();  // UDRE interrupt slows this way down on arduino1.0
+    //ADCSRA = 0xe5; // set the adc to free running mode
+    ADMUX = 0x40; // use adc0
+    //delay(1);
+    for (int i = 0 ; i < 90 ; i += 2) { // save 256 samples
+      while(!(ADCSRA & 0x10)); // wait for adc to be ready
+      ADCSRA = 0xf5; // restart adc
+      byte m = ADCL; // fetch adc data
+      byte j = ADCH;
+      int k = (j << 8) | m; // form into an int
+      k -= 0x0200; // form into a signed int
+      k <<= 6; // form into a 16b signed int
+      fft_input[i] = k; // put real data into even bins
+      fft_input[i+1] = 0; // set odd bins to 0
+    }
+    fft_window(); // window the data for better frequency response
+    fft_reorder(); // reorder the data before doing the fft
+    fft_run(); // process the data in the fft
+    fft_mag_log(); // take the output of the fft
+    sei();
+    //Serial.println("start");
+    for (byte i = 0 ; i < FFT_N/2 ; i++) { 
+      //Serial.println(fft_log_out[i]); // send out the data
+      output[i] = fft_log_out[i];
+    }
 
-    while(1) { // reduces jitter
-      //cli();  // UDRE interrupt slows this way down on arduino1.0
-      /*for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
-        while(!(ADCSRA & 0x10)); // wait for adc to be ready
-        ADCSRA = 0xf5; // restart adc
-        byte m = ADCL; // fetch adc data
-        byte j = ADCH;
-        int k = (j << 8) | m; // form into an int
-        k -= 0x0200; // form into a signed int
-        k <<= 6; // form into a 16b signed int
-        fft_input[i] = k; // put real data into even bins
-        fft_input[i+1] = 0; // set odd bins to 0
-      }
-      fft_window(); // window the data for better frequency response
-      fft_reorder(); // reorder the data before doing the fft
-      fft_run(); // process the data in the fft
-      fft_mag_log(); // take the output of the fft
-      sei();
-      //Serial.println("start");
-      for (byte i = 0 ; i < FFT_N/2 ; i++) { 
-        //Serial.println(fft_log_out[i]); // send out the data
-        output[i] = fft_log_out[i];
-      }*/
-      /*int maximum = 0;
-      int ind = 0;
-      for (byte i = 10; i < FFT_N/2; i++){
-        if (output[i] > maximum) {
-          maximum =  output[i];
-          ind = i;
+  s0_read = readQD(SENSOR0_PIN);
+  s1_read = readQD(SENSOR1_PIN);
+  //s2_read = readQD(SENSOR2_PIN);
+  if(s0_read > thresh0 && s1_read > thresh1)
+    goStraight();
+  else if(s0_read < thresh0 && s1_read > thresh1)
+    corRight();
+  else if(s0_read > thresh0 && s1_read < thresh1)
+    corLeft();
+  else if(s0_read < thresh0 && s1_read < thresh1){ // at an intersection
+      ADMUX = 0x41;
+      move_cw(myServo1, 0);
+      move_cw(myServo0, 0);
+      //delay(1);
+      int dist = adc_read();
+      //Serial.println(dist);
+      if (dist <= thresh_wall) goStraight();
+      else {
+        turnLeft(); // if wall in front, turn left
+        dist = adc_read();
+        if (dist <= thresh_wall) goStraight(); // if no wall to the left, go straight
+        else {
+          turnLeft();
+          dist = adc_read();;
+          if (dist <= thresh_wall) goStraight(); // if no wall behind, go straight
+          else {
+            turnLeft();
+            dist = adc_read();
+            if (dist <= thresh_wall) goStraight(); // if no wall to the right, go straight
+            else turnLeft();
+          }
         }
       }
-      Serial.println(maximum);
-      Serial.println(ind);
-      if(ind == 41 && maximum > 120){
-        //avoid other robot detected
-      }*/  
-      int dist = analogRead(DIST);
-      if (dist > 350) {
-        // stop
-        move_cw(myServo1, 0);
-        move_ccw(myServo2, 0);
-        // then turn left
-      }
-      /*else if (output[41] > 120) {
-        move_cw(myServo1, 0);
-        move_ccw(myServo2, 0);
-        // avoid other robot
-      }*/
-      else {
-        move_cw(myServo1, 50);
-        move_ccw(myServo2, 50);
-      }
-
+  }
   }
 }
