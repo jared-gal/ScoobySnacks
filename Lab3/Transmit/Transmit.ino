3b988ca1b57b8f33c05c9be5594fe1627d9eb6da
Larry Tang <ltang500@gmail.com>
2018-10-21 19:07:51 -0400

    transmit

--- Lab3/Transmit/Transmit.ino
+++ Lab3/Transmit/Transmit.ino
@@ -34,6 +34,8 @@ RF24 radio(9,10);
 
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint64_t pipes[2] = { 0x000000001aLL, 0x000000001bLL };
+static int x_start;
+static int y_start;
 
 //
 // Role management
@@ -58,7 +60,7 @@ void setup(void)
   // Print preamble
   //
 
-  Serial.begin(57600);
+  Serial.begin(9600);
   printf_begin();
   printf("\n\rRF24/examples/GettingStarted/\n\r");
   printf("ROLE: %s\n\r",role_friendly_name[role]);
@@ -116,6 +118,8 @@ void setup(void)
   //
 
   radio.printDetails();
+  x_start = 1;
+  y_start = 2;
 }
 
 unsigned long shiftpack(unsigned long x_coord, unsigned long y_coord, unsigned long pos_data) {
@@ -147,7 +151,7 @@ void loop(void)
   //
   // Ping out role.  Repeatedly send the current time
   //
-
+  
   if (role == role_ping_out)
   {
     // Run through a 3x3 maze
@@ -222,24 +226,12 @@ void loop(void)
     // Take the time, and send it.  This will block until complete
     //unsigned long time = millis();
     
-    /*printf("Now sending maze...");
-    unsigned char maze[3][3] =
-    {
-      3, 3, 3,
-      3, 1, 1,
-      3, 2, 0,
-    };
-    bool ok = radio.write( maze, sizeof(maze) );
-    if (ok)
-      printf("ok...\n");
-    else
-      printf("failed.\n\r");*/
 
     // Now, continue listening
     radio.startListening();
 
     // Wait here until we get a response, or timeout (250ms)
-    unsigned long started_waiting_at = millis();
+    /*unsigned long started_waiting_at = millis();
     bool timeout = false;
     while ( ! radio.available() && ! timeout )
       if (millis() - started_waiting_at > 200 )
@@ -258,53 +250,11 @@ void loop(void)
 
       // Spew it
       printf("Got response %lu, round-trip delay: %lu\n\r",got_time,millis()-got_time);
-    }
+    }*/
 
     // Try again 1s later
     delay(1000);
   }
-
-  //
-  // Pong back role.  Receive each packet, dump it out, and send it back
-  //
-
-  /*if ( role == role_pong_back )
-  {
-    // if there is data ready
-    if ( radio.available() )
-    {
-      // Dump the payloads until we've gotten everything
-      unsigned char maze;
-      bool done = false;
-      while (!done)
-      {
-        // Fetch the payload, and see if this was the last one.
-        done = radio.read( &got_time, sizeof(unsigned long) );
-
-        // Spew it
-        printf("Got payload %lu...",got_time);
-
-        // Delay just a little bit to let the other unit
-        // make the transition to receiver
-        delay(20);
-
-      }
-
-      // First, stop listening so we can talk
-      radio.stopListening();
-
-      // Send the final one back.
-      radio.write( &got_time, sizeof(unsigned long) );
-      printf("Sent response.\n\r");
-
-      // Now, resume listening so we catch the next packets.
-      radio.startListening();
-    }
-  }*/
-
-  //
-  // Change roles
-  //
 
   if ( Serial.available() )
   {
@@ -319,15 +269,6 @@ void loop(void)
       radio.openWritingPipe(pipes[0]);
       radio.openReadingPipe(1,pipes[1]);
     }
-//    else if ( c == 'R' && role == role_ping_out )
-//    {
-//      printf("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK\n\r");
-//
-//      // Become the primary receiver (pong back)
-//      role = role_pong_back;
-//      radio.openWritingPipe(pipes[1]);
-//      radio.openReadingPipe(1,pipes[0]);
-//    }
   }
 }
 // vim:cin:ai:sts=2 sw=2 ft=cpp
